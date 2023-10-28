2d5b0553d926bc14b6268d0842345cf1e69204e6
Hippolyte Verninas <etyl.sani@gmail.com>
2023-10-28 22:40:16 +0200

    fix: default shader

--- src/swarm_rescue/spg_overlay/gui_map/closed_playground.py
+++ src/swarm_rescue/spg_overlay/gui_map/closed_playground.py
@@ -26,7 +26,7 @@ class ClosedPlayground(Playground):
     """
     def __init__(self, size: Tuple[int, int]):
         background = (220, 220, 220)
-        use_shaders = False
+        use_shaders = True
         if platform.system() == "Darwin":
             use_shaders = False
 
