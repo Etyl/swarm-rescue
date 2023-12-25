b2658e56097115146692c84082d52aef08180dbc
Hippolyte Verninas <etyl.sani@gmail.com>
2023-12-25 23:34:11 +0100

    refactor

--- src/swarm_rescue/evaluator.py
+++ src/swarm_rescue/evaluator.py
@@ -2,6 +2,7 @@ import gc
 from typing import Tuple
 import multiprocessing
 import numpy as np
+import time
 
 from spg_overlay.entities.sensor_disablers import ZoneType
 from spg_overlay.utils.constants import DRONE_INITIAL_HEALTH
@@ -19,11 +20,33 @@ from maps.map_medium_02 import MyMapMedium02
 from solutions.my_drone_eval import MyDroneEval
 
 
+"""eval_config = EvalConfig(map_type=MyMapIntermediate01, nb_rounds=1)
+        self.eval_plan.add(eval_config=eval_config)
+
+        eval_config = EvalConfig(map_type=MyMapIntermediate02)
+        self.eval_plan.add(eval_config=eval_config)
+
+        zones_config: ZonesConfig = ()
+        eval_config = EvalConfig(map_type=MyMapMedium01, zones_config=zones_config, nb_rounds=1, config_weight=1)
+        self.eval_plan.add(eval_config=eval_config)
+
+        zones_config: ZonesConfig = (ZoneType.NO_COM_ZONE, ZoneType.NO_GPS_ZONE, ZoneType.KILL_ZONE)
+        eval_config = EvalConfig(map_type=MyMapMedium01, zones_config=zones_config, nb_rounds=1, config_weight=1)
+        self.eval_plan.add(eval_config=eval_config)
+
+        zones_config: ZonesConfig = (ZoneType.NO_COM_ZONE, ZoneType.NO_GPS_ZONE, ZoneType.KILL_ZONE)
+        eval_config = EvalConfig(map_type=MyMapMedium02, zones_config=zones_config, nb_rounds=1, config_weight=1)
+        self.eval_plan.add(eval_config=eval_config)
+"""
+
+
+
+
 class MyDrone(MyDroneEval):
     pass
 
 
-class Launcher:
+class Evaluator:
     """
     The Launcher class is responsible for running a simulation of drone rescue sessions. It creates an instance of the
     map with a specified zone type, constructs a playground using the construct_playground method of the map
@@ -50,19 +73,7 @@ class Launcher:
         Here you can fill in the evaluation plan ("evalplan") yourself, adding or removing configurations.
         A configuration is defined by a map of the environment and whether or not there are zones of difficulty.
         """
-
         self.team_info = TeamInfo()
-        self.eval_plan_single_drone = EvalPlan()
-
-        eval_config = EvalConfig(map_type=MyMapIntermediate01, nb_rounds=1)
-        self.eval_plan_single_drone.add(eval_config=eval_config)
-
-        self.number_drones = None
-        self.time_step_limit = None
-        self.real_time_limit = None
-        self.number_wounded_persons = None
-        self.size_area = None
-        self.score_manager = None
 
     def one_round(self, eval_config: EvalConfig):
         """
@@ -73,16 +84,16 @@ class Launcher:
         the images and data related to the round.
         """
         my_map = eval_config.map_type(eval_config.zones_config)
-        self.number_drones = my_map.number_drones
-        self.time_step_limit = my_map.time_step_limit
-        self.real_time_limit = my_map.real_time_limit
-        self.number_wounded_persons = my_map.number_wounded_persons
-        self.size_area = my_map.size_area
+        number_drones = my_map.number_drones
+        time_step_limit = my_map.time_step_limit
+        real_time_limit = my_map.real_time_limit
+        number_wounded_persons = my_map.number_wounded_persons
+        size_area = my_map.size_area
 
-        self.score_manager = ScoreManager(number_drones=self.number_drones,
-                                          time_step_limit=self.time_step_limit,
-                                          real_time_limit=self.real_time_limit,
-                                          total_number_wounded_persons=self.number_wounded_persons)
+        self.score_manager = ScoreManager(number_drones=number_drones,
+                                          time_step_limit=time_step_limit,
+                                          real_time_limit=real_time_limit,
+                                          total_number_wounded_persons=number_wounded_persons)
 
         playground = my_map.construct_playground(drone_type=MyDrone)
 
@@ -106,44 +117,51 @@ class Launcher:
                 my_gui.real_time_limit_reached)
 
     def evaluate_single_drone(self, id: int = 0):
+        gc.disable()
+
         """
         The go method in the Launcher class is responsible for running the simulation for different eval_config,
          and calculating the score for each one.
         """
 
-        for eval_config in self.eval_plan_single_drone.list_eval_config:
-            gc.collect()
-            print("")
+        eval_config = EvalConfig(map_type=MyMapIntermediate01, nb_rounds=1)
+        my_map = eval_config.map_type(eval_config.zones_config)
+        number_wounded_persons = my_map.number_wounded_persons
 
-            if not isinstance(eval_config.zones_config, Tuple) and not isinstance(eval_config.zones_config[0], Tuple):
-                raise ValueError("Invalid eval_config.zones_config. It should be a tuple of tuples of ZoneType.")
+        gc.collect()
+        print("")
 
-            print(f"*** Map: {eval_config.map_name}, special zones: {eval_config.zones_name_casual}")
+        if not isinstance(eval_config.zones_config, Tuple) and not isinstance(eval_config.zones_config[0], Tuple):
+            raise ValueError("Invalid eval_config.zones_config. It should be a tuple of tuples of ZoneType.")
 
-            gc.collect()
-            result = self.one_round(eval_config)
-            (percent_drones_destroyed, mean_drones_health, elapsed_time_step, rescued_all_time_step,
-                score_exploration, rescued_number, real_time_elapsed, real_time_limit_reached) = result
+        print(f"*** Map: {eval_config.map_name}, special zones: {eval_config.zones_name_casual}")
 
-            result_score = self.score_manager.compute_score(rescued_number,
-                                                            score_exploration,
-                                                            rescued_all_time_step)
-            (round_score, percent_rescued, score_time_step) = result_score
+        gc.collect()
+        result = self.one_round(eval_config)
+        (percent_drones_destroyed, mean_drones_health, elapsed_time_step, rescued_all_time_step,
+            score_exploration, rescued_number, real_time_elapsed, real_time_limit_reached) = result
 
-            if real_time_limit_reached:
-                return None
-            
-            return (round_score, score_exploration, rescued_number/self.number_wounded_persons, rescued_all_time_step, mean_drones_health)
+        result_score = self.score_manager.compute_score(rescued_number,
+                                                        score_exploration,
+                                                        rescued_all_time_step)
+        (round_score, percent_rescued, score_time_step) = result_score
+
+        if real_time_limit_reached:
+            return None
+        
+        return (round_score, score_exploration, rescued_number/number_wounded_persons, rescued_all_time_step, mean_drones_health, elapsed_time_step / real_time_elapsed)
 
             
 if __name__ == "__main__":
-    gc.disable()
-    launcher = Launcher()
+    evaluator = Evaluator()
     pool = multiprocessing.Pool(4)
-    results = pool.map(launcher.evaluate_single_drone, range(4))
+    t0 = time.time()
+    results = pool.map(evaluator.evaluate_single_drone, range(16))
+    t = time.time() - t0
 
     results_avg = np.mean(results, axis=0)
     print("Score: ", results_avg[0])
     print("Exploration: ", results_avg[1])
     print("Rescued: ", results_avg[2])
     print("Rescue time: ", results_avg[3])
+    print("Total time: ", t)
