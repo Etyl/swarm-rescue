c520de694f5e6125e12955f9eae448b9649a95e0
Etyl <etyl.sani@gmail.com>
2024-12-15 01:39:28 +0000

    refactor: used multiprocessing pool for benchmarking

--- src/swarm_rescue/run_launcher.py
+++ src/swarm_rescue/run_launcher.py
@@ -6,10 +6,10 @@ from spg_overlay.reporting.result_path_creator import ResultPathCreator
 from spg_overlay.reporting.team_info import TeamInfo
 import uuid
 import os
+import multiprocessing
 
-def launch_parallel_task(
-    script_name, map_name, name, result_path, zone_type, nb_rounds, video_capture_enabled=False
-):
+def launch_parallel_task(args):
+    script_name, map_name, name, result_path, zone_type, video_capture_enabled = args
     os.makedirs(result_path, exist_ok=True)
     log_file_path = os.path.join(result_path, "log.txt")
     with open(log_file_path, "w") as log_file:
@@ -23,8 +23,6 @@ def launch_parallel_task(
                 name,
                 "--zone",
                 zone_type,
-                "--rounds",
-                str(nb_rounds),
                 "--video",
                 str(video_capture_enabled),
                 "--result_path",
@@ -35,51 +33,57 @@ def launch_parallel_task(
 
 
 if __name__ == "__main__":
-
     team_info = TeamInfo()
     rpc = ResultPathCreator(team_info)
 
     name = rpc.path
+
+    round_count = 20
     map_names = [
-        ("MyMapIntermediate01", "NO_GPS_ZONE", 3),
-        ("MyMapIntermediate01", "NONE", 2),
-        ("MyMapIntermediate01", "NO_GPS_ZONE", 1),
+        ("MyMapFinal2022_23", "NONE"),
+        ("MyMapFinal2022_23", "NO_GPS_ZONE"),
+        ("MyMapFinal2022_23", "NO_COM_ZONE"),
+        ("MyMapFinal2022_23", "KILL_ZONE"),
+        ("MyMapFinal_2023_24_01", "NONE"),
+        ("MyMapFinal_2023_24_02", "NONE"),
+        ("MyMapFinal_2023_24_03", "NONE"),
+        ("MyMapFinal_2023_24_01", "NO_COM_ZONE"),
+        ("MyMapFinal_2023_24_02", "KILL_ZONE"),
+        ("MyMapFinal_2023_24_03", "NO_GPS_ZONE")
     ]
+
+    complete_map_names = []
+    for map_name in map_names:
+        complete_map_names = complete_map_names + [map_name]*round_count
     video_capture_enabled = True
 
     script_name = "src/swarm_rescue/parallel_launcher.py"
 
-    threads = []
-    for map_name, zone_type, nb_rounds in map_names:
-        # random id to avoid conflicts
+    # Prepare task arguments
+    tasks = []
+    for map_name, zone_type in map_names:
         result_path = (
-            name 
+            name
             + "/"
             + str(uuid.uuid4())
             + "_map_"
             + map_name
             + "_zone_"
             + zone_type
-            + "_rounds_"
-            + str(nb_rounds)
             + "/"
         )
-        thread = threading.Thread(
-            target=launch_parallel_task,
-            args=(
-                script_name,
-                map_name,
-                name,
-                result_path,
-                zone_type,
-                nb_rounds,
-                video_capture_enabled,
-            ),
-        )
-        threads.append(thread)
-        thread.start()
+        tasks.append((
+            script_name,
+            map_name,
+            name,
+            result_path,
+            zone_type,
+            video_capture_enabled,
+        ))
 
-    for thread in threads:
-        thread.join()
+    num_workers = min(len(tasks), 10)
+
+    with multiprocessing.Pool(processes=num_workers) as pool:
+        pool.map(launch_parallel_task, tasks)
 
     print("All run information saved in: ", name)
