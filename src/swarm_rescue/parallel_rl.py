5ce37b46948a59039f48d18eaa7c30a6708f2b15
Etyl <etyl.sani@gmail.com>
2025-02-26 00:06:01 +0000

    feat: parallel launcher

---
+++ src/swarm_rescue/parallel_rl.py
@@ -0,0 +1,36 @@
+import argparse
+import gc
+import os
+
+from maps.map_final_2022_23 import MyMapFinal2022_23
+from maps.map_final_2023_24_01 import MyMapFinal_2023_24_01
+from maps.map_final_2023_24_02 import MyMapFinal_2023_24_02
+from maps.map_final_2023_24_03 import MyMapFinal_2023_24_03
+from maps.map_medium_01 import MyMapMedium01
+from maps.map_medium_02 import MyMapMedium02
+
+
+if __name__ == "__main__":
+    gc.disable()
+    parser = argparse.ArgumentParser(description="RL launcher")
+    parser.add_argument("--map", "-m", required=True, help="Map to run")
+    parser.add_argument("--epsilon", "-n", required=True, help="Epsilon greedy parameter")
+    parser.add_argument("--output", "-rp", required=True, help="Path to save the results")
+    args = parser.parse_args()
+
+    maps = [
+        MyMapFinal2022_23,
+        MyMapFinal_2023_24_01,
+        MyMapFinal_2023_24_02,
+        MyMapFinal_2023_24_03,
+        MyMapMedium01,
+        MyMapMedium02
+    ]
+
+    maps_dict = {map.__name__: map for map in maps}
+
+    map_type = maps_dict[args.map]
+
+    output_path = args.output
+
+    get_run(policy, output_path, map_type)
---
+++ src/swarm_rescue/rl_launcher.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import time
+import uuid
+import subprocess
+from datetime import datetime
+from multiprocessing import Pool
+
+from maps.map_final_2022_23 import MyMapFinal2022_23
+from maps.map_final_2023_24_01 import MyMapFinal_2023_24_01
+from maps.map_final_2023_24_02 import MyMapFinal_2023_24_02
+from maps.map_final_2023_24_03 import MyMapFinal_2023_24_03
+from maps.map_medium_01 import MyMapMedium01
+from maps.map_medium_02 import MyMapMedium02
+
+
+def convert_seconds(seconds):
+    return time.strftime("%H:%M:%S", time.gmtime(seconds))
+
+
+def run_simulation(task):
+    """Run a single simulation task as a subprocess."""
+    map_name, epsilon, file_path = task
+    os.makedirs(file_path, exist_ok=True)
+    python_path = os.path.dirname(os.path.abspath(__file__))
+    python_path = os.path.join(python_path, "parallel_rl.py")
+    subprocess.run(
+        [
+            sys.executable,
+            python_path,  # Replace with your actual script name
+            "--map",
+            map_name,
+            "--epsilon",
+            epsilon,
+            "--output",
+            file_path,
+        ]
+    )
+
+
+if __name__ == "__main__":
+    maps = [
+        MyMapFinal2022_23,
+        MyMapFinal_2023_24_01,
+        MyMapFinal_2023_24_02,
+        MyMapFinal_2023_24_03,
+        MyMapMedium01,
+        MyMapMedium02,
+    ]
+
+    date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    main_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"rl-run_{date}")
+    os.makedirs(main_dir, exist_ok=True)
+
+    test_rounds = 15
+    epsilon_rounds = 35
+    tasks = []
+    for map_class in maps:
+        epsilon = lambda t: 0.4 * (epsilon_rounds - 1 - t) / (epsilon_rounds - 1)
+        
+        for k in range(test_rounds):
+            file_path = os.path.join(
+                main_dir,
+                f"{map_class.__name__}_0_{uuid.uuid4()}"
+            )
+            tasks.append((map_class.__name__, "0.0", file_path))
+        
+        for k in range(epsilon_rounds):
+            file_path = os.path.join(
+                main_dir,
+                f"{map_class.__name__}_{round(epsilon(k),3)}_{uuid.uuid4()}"
+            )
+            tasks.append((map_class.__name__, str(round(epsilon(k),3)), file_path))
+
+    num_workers = min(len(tasks), 20)
+    time_start = time.time()
+
+    print(f"Starting {len(tasks)} tasks with {num_workers} workers.")
+
+    with Pool(processes=num_workers) as pool:
+        pool.map(run_simulation, tasks)
+
+    print(f"Finished all tasks in {convert_seconds(time.time() - time_start)}")
+    print(f"All results saved in: {main_dir}")
